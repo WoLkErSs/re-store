55d8059391c9b6f62b162626485fa3369dd2ca1c
WoLkErSs <ivanpauls1705@gmail.com>
2024-10-01 14:36:26 +0200

    ownProps was used

--- src/components/books-list/books-list.js
+++ src/components/books-list/books-list.js
@@ -10,15 +10,7 @@ import ErrorIndicator from '../error-indicator';
 
 class BooksList extends Component {
   componentDidMount() {
-    const {
-      bookstoreService,
-      booksLoaded,
-      booksRequested,
-      booksError } = this.props;
-    booksRequested();
-    bookstoreService.getBooks()
-      .then((data) => { booksLoaded(data) })
-      .catch((error) => { booksError(error) });
+    this.props.fetchBooks();
   }
   render() {
     const { books, loading, error } = this.props
@@ -47,8 +39,18 @@ const mapStateToProps = ({books, loading, error}) => {
   return { books, loading, error }
 }
 
-const mapDispatchToProps = {
-  booksLoaded, booksRequested, booksError
+const mapDispatchToProps = (dispatch, ownProps) => {
+  const { bookstoreService } = ownProps
+  return(
+    {
+      fetchBooks: () => {
+        dispatch(booksRequested());
+        bookstoreService.getBooks()
+          .then((data) => { dispatch(booksLoaded(data)) })
+          .catch((error) => { dispatch(booksError(error)) });
+      }
+    }
+  )
 }
 
 export default compose(
